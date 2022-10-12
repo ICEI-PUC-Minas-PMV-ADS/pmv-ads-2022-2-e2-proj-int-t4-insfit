08de1e4ec1577394768f0a1e6eb32c91680b43ae
mthszf99 <matheus.pires@sga.pucminas.br>
2022-10-12 20:18:25 -0300

    inclusao de campos do feed

--- INSFIT/Models/Feed.cs
+++ INSFIT/Models/Feed.cs
@@ -1,6 +1,18 @@
-﻿namespace INSFIT.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using MessagePack;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+
+namespace INSFIT.Models
 {
     public class Feed
     {
+        [Key]
+        public int Id_Feed { get; set; }
+        public string CampoTexto { get; set; }
+        public string CampoImgem { get; set; }
+        public string CampodePesquisa { get; set; }
+        public string Data { get; set; }
+        public string Comentario { get; set; }
     }
 }
