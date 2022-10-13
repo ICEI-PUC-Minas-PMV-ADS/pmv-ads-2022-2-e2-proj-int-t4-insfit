3487c2ad8829c07e273b3b5dc5ec0cde26e0d659
mthszf99 <matheus.pires@sga.pucminas.br>
2022-10-13 20:03:10 -0300

    dbcontext feed

---
+++ INSFIT/Migrations/20221013230007_F01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INSFIT.Migrations
+{
+    public partial class F01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feed",
+                columns: table => new
+                {
+                    Id_Feed = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CampoTexto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CampoImgem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CampodePesquisa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataPublicacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Comentario = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feed", x => x.Id_Feed);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feed");
+        }
+    }
+}
