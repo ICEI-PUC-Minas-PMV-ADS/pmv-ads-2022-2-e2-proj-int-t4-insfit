19c2f0abefd7fb97aa49cb77b5319e254f8f9188
Matheus Sampaio <matheuslemos98@gmail.com>
2022-11-03 21:10:38 -0300

    Ajuste Cadsatro e Captura de localização do usuário.

---
+++ INSFIT/wwwroot/js/Mapa.js
@@ -0,0 +1,13 @@
+﻿let h2 = document.querySelector('h2');
+
+// Captando a latidude e longitude do usuário
+function success(pos) {
+    console.log(pos.coords.latitude, pos.coords.longitude);
+    //h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+}
+
+function error(err) {
+    console.log(err);
+}
+
+navigator.geolocation.getCurrentPosition(success, error); 
