ba18e956cd6c843db18f18233fbd472256544ce0
Vitor Hugo <vitorhugoemail@gmail.com>
2022-11-07 19:07:52 -0300

    detalhes no front

--- INSFIT/wwwroot/js/site.js
+++ INSFIT/wwwroot/js/site.js
@@ -9,8 +9,7 @@ $('#tabela-usuarios').DataTable({
     "searching": true,
     "oLanguage": {
         "sEmptyTable": "Nenhum registro encontrado na tabela",
-        "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
-        "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+       // "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
         "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
         "sInfoPostFix": "",
         "sInfoThousands": ".",
