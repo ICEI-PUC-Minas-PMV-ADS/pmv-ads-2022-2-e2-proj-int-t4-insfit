08cb4244ad3961a949be3dfc6e7db46981f1dabd
Matheus Sampaio <matheuslemos98@gmail.com>
2022-11-09 21:11:44 -0300

    Apresentação do mapa com a localização do usuário

--- INSFIT/wwwroot/js/Mapa.js
+++ INSFIT/wwwroot/js/Mapa.js
@@ -1,25 +1,34 @@
-﻿/*let h2 = document.querySelector('h2');
+﻿let h2 = document.querySelector('h2');
+var map;
 
 // Captando a latidude e longitude do usuário
 function success(pos) {
     console.log(pos.coords.latitude, pos.coords.longitude);
     //h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+
+    if (map === undefined) {
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 15);
+    } else {
+        map.remove();
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 15);
+    }
+
+    //função para renderizar o mapa
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    //função para criar o marcador do mapa
+    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+        .bindPopup('Esta é minha localização.')
+        .openPopup();
+
 }
 
-function error(err) {
-    console.log(err);
-}
+ navigator.geolocation.getCurrentPosition(success);
 
-var watchID = navigator.geolocation.watchPosition(success, error, {
-    enableHighAccuracy = true;
-}); */
 
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+   
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+
