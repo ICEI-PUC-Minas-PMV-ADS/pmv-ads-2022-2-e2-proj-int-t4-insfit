a45a4ba9c048d85ac0b2815b5517a97491a8c2ea
Vicinius115 <viciniuseduardo887@gmail.com>
2022-11-23 23:03:37 -0300

    atualização campos de acrescimo de dados e inicio do css

--- INSFIT/wwwroot/js/Relatorio.js
+++ INSFIT/wwwroot/js/Relatorio.js
@@ -5,19 +5,20 @@
     var peso = document.getElementById("peso").value
 
     var imc = peso / (altura) ** 2
-
+    var text=""
     console.log(imc)
     if (imc < 18.5) {
-        window.alert("Você está magro, visite a pagina dietas")
+        text="Você está magro, visite a pagina dietas."
     } else if (imc < 24.9) {
-        window.alert("Você está normal, visite a pagina dietas")
+        text="Você está normal, visite a pagina dietas."
     }  else if (imc < 29.9) {
-        window.alert("Você está com sobrepeso, visite a pagina dietas")
+        text="Você está com sobrepeso, visite a pagina dietas."
     } else if (imc < 39.9) {
-        window.alert("Você está com obesidade, visite a pagina dietas")
+        text="Você está com obesidade, visite a pagina dietas."
     } else if (imc > 39.9) {
-        window.alert("Você está com obesidade mórbida, visite a pagina dietas")
+        text="Você está com obesidade mórbida, visite a pagina dietas."
     }
+    document.getElementById("text_area").innerText=text
 
 
 }
