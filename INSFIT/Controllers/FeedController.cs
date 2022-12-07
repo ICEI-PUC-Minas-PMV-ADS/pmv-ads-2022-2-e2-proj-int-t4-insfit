a0d8f0cd94dd5e7800f3fd1472b89657b3cd2470
mthszf99 <matheus.pires@sga.pucminas.br>
2022-12-07 09:57:55 -0300

    feed nao posta (teste)

--- INSFIT/Controllers/FeedController.cs
+++ INSFIT/Controllers/FeedController.cs
@@ -7,27 +7,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using INSFIT.Data;
 using INSFIT.Models;
-using Microsoft.AspNetCore.Http;
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
+
 
 namespace INSFIT.Controllers
 {
     public class FeedController : Controller
     {
         private readonly INSFITContext _context;
-        private string _filePath;
 
-        public FeedController(INSFITContext context, IWebHostEnvironment env)
+        public FeedController(INSFITContext context)
         {
-            _filePath = env.WebRootPath;
             _context = context;
         }
 
         // GET: Feed
         public async Task<IActionResult> Index()
         {
-              ViewBag.Path = _filePath;
               return View(await _context.Feed.ToListAsync());
         }
 
@@ -60,57 +55,16 @@ namespace INSFIT.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id_Feed,CampoTexto,CampoImgem,DataPublicacao,Comentario")] Feed feed, IformFile fotos)
+        public async Task<IActionResult> Create([Bind("Id_Feed,CampoTexto,CampoImgem,DataPublicacao,Comentario")] Feed feed)
         {
             if (ModelState.IsValid)
-            {
-                if (!ValidaImagem(fotos))
-                    return View(feed);
-
-                var nome = SalvarArquivo(fotos);
-                feed.CampoImgem = nome;
-                
-                
+            {                            
                 _context.Add(feed);
                 await _context.SaveChangesAsync();
 
                 return RedirectToAction(nameof(Index));
             }
             return View(feed);
-        }
-
-        public bool ValidaImagem(IformFile fotos)
-        {
-            switch (fotos.ContentType)
-            {
-                case "image/jpeg":
-                    return true;
-                case "image/bmp":
-                    return true;
-                case "image/gif":
-                    return true;
-                case "image/png":
-                    return true;
-                default:
-                    return false;
-                    break;
-            }
-        }
-        public string SalvarArquivo(IformFile fotos)
-        {
-            var nome = Guid.NewGuid().ToString() + fotos.FileName;
-
-            var filePath = _filePath + "\\imagens";
-            if (Directory.Exists(filePath))
-            {
-                Directory.CreateDirectory(filePath);
-            }
-            using (var stream = System.IO.File.Create(filePath + "\\" + nome))
-            {
-                fotos.CopyToAsync(stream);
-            }
-
-            return nome;
         }
 
         // GET: Feed/Edit/5
@@ -192,10 +146,6 @@ namespace INSFIT.Controllers
                 return Problem("Entity set 'INSFITContext.Feed'  is null.");
             }
             var feed = await _context.Feed.FindAsync(id);
-            string filePathName = _filePath + "\\imagens" + feed.CampoImgem;
-
-            if (System.IO.File.Exists(filePathName))
-                System.IO.File.Delete(filePathName);
 
             if (feed != null)
             {
@@ -210,16 +160,6 @@ namespace INSFIT.Controllers
         {
           return _context.Feed.Any(e => e.Id_Feed == id);
         }
-        public class IformFile
-        {
-            internal string ContentType;
 
-            public string FileName { get; internal set; }
-
-            internal void CopyToAsync(FileStream stream)
-            {
-                throw new NotImplementedException();
-            }
-        }
     }
 }
