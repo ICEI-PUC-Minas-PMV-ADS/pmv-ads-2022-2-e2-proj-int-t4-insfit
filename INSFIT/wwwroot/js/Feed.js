5e43d9656131b9318186410097ff482f3fea7000
mthszf99 <matheus.pires@sga.pucminas.br>
2022-12-09 08:53:52 -0300

    update feed

--- INSFIT/wwwroot/js/Feed.js
+++ INSFIT/wwwroot/js/Feed.js
@@ -1,4 +1,4 @@
-﻿const button = document.querySelector(".btn");
+﻿const button = document.querySelector(".btn-pag");
 
 button.addEventListener('click', function () {
     button.classList.toggle('curtido');
